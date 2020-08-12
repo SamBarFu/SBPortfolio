dab20a0283f0c90d8b4720dafc1c9fb2c05ad9d1
SamBarFu <samuelbarberena12@gmail.com>
2020-08-11 22:20:19 -0600

    Revert "section About and Education"
    This reverts commit 5f444fd596096a4e0583ca3ed2cda13daa3a0461.

--- sbportfolio/src/components/version 2.0/resume/Resume.js
+++ sbportfolio/src/components/version 2.0/resume/Resume.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react'
 
 /* components import */
 import Header from './Header'
-import About from './About'
-import Education from './Education'
 
 class Resume extends Component{
     render(){
         return <div>
-            <Header/>
-            <About/>
-            <Education/>
+            <Header></Header>
         </div>
     }
 }
